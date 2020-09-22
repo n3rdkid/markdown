f8cd054cf331ec61086b3fc2a95eb6215237bd3a
Saurav Adhikari <sauravads123@gmail.com>
2020-09-22 21:38:29 +0545

    fix : storybook not working because of redux

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,10 +2,12 @@
 import { addDecorator } from '@storybook/react';
 import React from "react"
 import { withConsole } from '@storybook/addon-console';
-import { MemoryRouter } from "react-router-dom"
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-addDecorator((storyFn, context) => <MemoryRouter>{withConsole()(storyFn)(context)}</MemoryRouter>);
+addDecorator((storyFn, context) => <MemoryRouter><Provider store={store}>{withConsole()(storyFn)(context)}</Provider></MemoryRouter>);
